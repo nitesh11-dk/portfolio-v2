54a8243addf91157afaf9e00d4b84a1c03617282
Nitesh-DK <nittesh.dk@gmail.com>
2025-06-13 23:31:57 +0530

    Update Hero section styles; adjusted margin and text color for improved visibility and aesthetics.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,11 +14,11 @@ const Hero = () => {
 
   return (
     <section className="min-h-screen w-full  flex flex-col relative" id="home">
-      <div className="w-full z-10 mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <div className="w-full z-10 mx-auto mt-40 flex flex-col sm:mt-36  c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Nitesh <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-zinc-200">Building Brands, Developing Web</p>
+        <p className="hero_tag text-zinc-100 md:text-zinc-200">Building Brands, Developing Web</p>
       </div>
 
       <div className="w-full h-full absolute  inset-0">
